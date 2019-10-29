97338bf6b58042d2062755f30be8be8d9dbbb1ee
Anshul Gupta <anshulg954@outlook.com>
2019-10-29 22:53:12 +0530

    Basic func

---
+++ 21.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+void techhunt(int &i, int j) {
+ j = 1100;
+ i = 0120;
+}
+int main () {
+ int apratim2k19 =11;
+ techhunt (apratim2k19,apratim2k19);
+ cout <<apratim2k19<< endl;
+ return 0;
+}
