0a16e4525c52c5ecf151e43b3434cd9812ae2272
Anshul Gupta <anshulg954@outlook.com>
2019-10-29 22:49:01 +0530

    FINAL CLUE IF ELSE BASIC KNOWLEDGE

---
+++ 14.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+    int main()
+    {
+        float f1 = 0.1;
+        if (f1 == 0.1)
+            printf("GO TO CLUE\n");
+        else
+            printf("GO TO CLUE X\n");
+    }
