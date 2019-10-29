8ed2afa22d754f2d6ad97757447c4f2e0f41f4ab
Anshul Gupta <anshulg954@outlook.com>
2019-10-29 22:50:20 +0530

    LIMITS IN C

---
+++ 15.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main(int argc, char** argv) {
+    printf("a    =   %d\n", CHAR_BIT);
+    printf("b    =   %d\n", SCHAR_MAX);
+    printf("c    =   %d\n", SCHAR_MIN);
+    printf("d    =   %d\n", UCHAR_MAX);
+    return 0;
+}
