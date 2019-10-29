7d43f61a7b369418a6025994528bf7252ec2f456
Anshul Gupta <anshulg954@outlook.com>
2019-10-29 22:44:49 +0530

    BASIC IF ELSE

---
+++ 6.C
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+
+{
+
+  int x;
+
+  for(x=-1; x<=10; x++)
+
+  {
+
+    if(x < 5)
+
+      continue;
+
+    else
+
+      break;
+
+    printf("WELCOME TO TECH HUNT 2K19!");
+
+  }
+
+  return 0;
+
+}
